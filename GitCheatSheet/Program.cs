56e5b4936d52fcc710c5caa49cbfba3fffa6a6d2
Micael <sjodin.micael.gmail.com>
2024-03-21 14:00:39 +0100

    Reset - step 1 done

--- GitCheatSheet/Program.cs
+++ GitCheatSheet/Program.cs
@@ -7,46 +7,7 @@ namespace GitCheatSheet
     {
         static void Main(string[] args)
         {
-            List<string> commands = new List<string>();
-
-
-            commands.Add("git init");
-            commands.Add("git clone [repository URL]");
-            commands.Add("git add [file(s)]");
-            commands.Add("git commit -m \"[commit message]\"");
-            commands.Add("git status");
-            commands.Add("git diff");
-            commands.Add("git diff --staged");
-            commands.Add("git reset [file(s)]");
-            commands.Add("git checkout -- [file(s)]");
-            commands.Add("git branch");
-            commands.Add("git branch [branch_name]");
-            commands.Add("git checkout [branch_name]");
-            commands.Add("git merge [branch_name]");
-            commands.Add("git remote");
-            commands.Add("git remote add [name] [URL]");
-            commands.Add("git fetch [remote]");
-            commands.Add("git pull [remote] [branch]");
-            commands.Add("git push [remote] [branch]");
-            commands.Add("git clean");
-            commands.Add("git stash pop");
-            commands.Add("git stash");
-            commands.Add("git push--tags");
-            commands.Add("git tag");
-            commands.Add("git tag -a [tag_name] \"[tag_message]\"");
-            commands.Add("git tag [tag name]");
-            commands.Add("git show [commit hash]");
-            commands.Add("git log--author=[author]");
-            commands.Add("git log--grep=[pattern]");
-            commands.Add("git fetch [remote]");
-            commands.Add("git pull [remote] [branch]");
-            commands.Add("git push [remote] [branch]");
-            commands.Add("git log");
-            commands.Add("git log --online");
-
-            string gitCommands = JsonSerializer.Serialize(commands);
             string path = Path.Combine(AppContext.BaseDirectory, "Comands.json");
-            File.WriteAllText(path, gitCommands);
         }
     }
 }
