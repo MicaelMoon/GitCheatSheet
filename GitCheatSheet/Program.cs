ce4c7a498a86f867d5791ed4427543454c2e1715
fransisco7117 <fransisco7117@gmail.com>
2024-03-21 09:24:52 +0100

    Added List

--- GitCheatSheet/Program.cs
+++ GitCheatSheet/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace GitCheatSheet
+﻿using System.Threading.Channels;
+
+namespace GitCheatSheet
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            List<string> commands = new List<string>();
         }
     }
 }
